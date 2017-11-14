6b9ad84cea73430020555d87536cb6caa0f6d7fb
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2017-11-13 23:49:47 -0500

    Properly bypass the settings...

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 # circle CI doesn't have rsync installed
 sudo apt install -y rsync
 # https://ubuntuforums.org/showthread.php?t=1628566
-rsync -o StrictHostKeyChecking=no -a -e "ssh" --rsync-path="sudo rsync" -rv dist/ ec2-user@goonytoons.com:/var/www/html
+rsync -a -e "ssh -o StrictHostKeyChecking=no" --rsync-path="sudo rsync" -rv dist/ ec2-user@goonytoons.com:/var/www/html
