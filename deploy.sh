3c40e9a0d159b289453275ca4924ec5d58d4d584
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2017-11-13 23:47:38 -0500

    Add bypass for user prompt

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+# circle CI doesn't have rsync installed
+sudo apt install -y rsync
 # https://ubuntuforums.org/showthread.php?t=1628566
-rsync -a -e "ssh" --rsync-path="sudo rsync" -rv dist/ ec2-user@goonytoons.com:/var/www/html
+rsync -o StrictHostKeyChecking=no -a -e "ssh" --rsync-path="sudo rsync" -rv dist/ ec2-user@goonytoons.com:/var/www/html
