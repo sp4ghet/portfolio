09a15fb813b3cdd379308ec4995f883d8e09fd68
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-08-04 19:38:26 +0900

    Adjust the feeling of the webpage, mainly adjusting font size and line breaks

--- src/About/View.elm
+++ src/About/View.elm
@@ -23,28 +23,27 @@ root model =
 
 contents : Model -> Html Msg
 contents model =
-    section
-        [ class "section"
-        , style [ ( "padding", "0 10vw" ) ]
+    div
+        [ class "article-container"
+        , class "container "
         ]
-        [ div [ class "article-container" ]
-            [ div
-                [ id "about-me-contents"
-                , class "container content"
-                , style
-                    [ ( "width", "80%" )
-                    , ( "padding", "3vh 0" )
-                    ]
+        [ div
+            [ id "about-me-contents"
+            , class "content"
+            , class "container is-widescreen"
+            , style
+                [ ( "width", "80%" )
+                , ( "padding", "3vh 0" )
                 ]
-                [ h1 [ class "is-title" ] [ text "Rikuo Hasegawa // 長谷川陸央 // sp4ghet" ]
-                , div [] [ img [ src "assets/img/icons/icon.png" ] [] ]
-                , div []
-                    [ a [ href model.twitterUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-social-twitter-outline" ] [] ]
-                    , a [ href model.facebookUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-social-facebook-outline" ] [] ]
-                    , a [ href model.githubUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-social-github-outline" ] [] ]
-                    , a [ href model.bookmeterUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-ios-bookmarks-outline" ] [] ]
-                    ]
-                , Markdown.toHtml [] model.description
+            ]
+            [ h1 [ class "is-title" ] [ text "Rikuo Hasegawa // 長谷川陸央 // sp4ghet" ]
+            , div [] [ img [ src "assets/img/icons/icon.png" ] [] ]
+            , div []
+                [ a [ href model.twitterUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-social-twitter-outline" ] [] ]
+                , a [ href model.facebookUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-social-facebook-outline" ] [] ]
+                , a [ href model.githubUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-social-github-outline" ] [] ]
+                , a [ href model.bookmeterUrl, class "icon has-text-link", target "_blank" ] [ i [ class "ion-ios-bookmarks-outline" ] [] ]
                 ]
+            , Markdown.toHtml [ class "is-size-5-desktop" ] model.description
             ]
         ]
