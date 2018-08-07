fb6f52ee8af230e7283b24aaa407211b0c8fac66
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-08-07 14:30:28 +0900

    Begin working on blog

--- src/Blog/State.elm
+++ src/Blog/State.elm
@@ -1,11 +1,15 @@
 module Blog.State exposing (update, init)
 
 import Blog.Types exposing (..)
+import Common.State as Navbar
+import Blog.Article.Types exposing (Article)
 
 
 init : Model
 init =
     Model
+        Navbar.initNav
+        []
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
