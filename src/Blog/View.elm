b35030379fa64e3d759bbf2e1859c0e4c4eace2c
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-11-21 13:35:32 +0000

    save work

--- src/Blog/View.elm
+++ src/Blog/View.elm
@@ -1,12 +1,41 @@
 module Blog.View exposing (root)
 
 import Blog.Types exposing (..)
-import Html exposing (..)
+import Blog.Article.Types as Article
 import Common.ViewComponents exposing (navBar)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Date
 
 
 root : Model -> Html Msg
 root model =
-    div []
-        [ Html.map Nav (navBar model.navModel)
-        ]
+    let
+        summary : Article.Model -> Html msg
+        summary articleModel =
+            let
+                article =
+                    articleModel.article
+
+                dateString =
+                    case article.postDate of
+                        Ok date ->
+                            toString (Date.month date) ++ "/" ++ toString (Date.day date) ++ "/" ++ toString (Date.year date)
+
+                        Err err ->
+                            "no date"
+            in
+                div []
+                    [ h3 [] [ text article.title ]
+                    , span [] [ text dateString ]
+                    , div []
+                        [ p [] [ text article.summary ]
+                        , a [ href ("#blog/" ++ article.id) ] [ text "read more..." ]
+                        ]
+                    ]
+    in
+        div []
+            [ Html.map Nav
+                (navBar model.navModel)
+            , div [] (List.map summary model.articles)
+            ]
