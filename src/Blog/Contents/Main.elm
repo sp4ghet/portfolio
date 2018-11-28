6606461c7ea92403603a1b89df2219170a2a529a
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-11-28 20:33:44 +0000

    Implement articles-view page

--- src/Blog/Contents/Main.elm
+++ src/Blog/Contents/Main.elm
@@ -2,8 +2,11 @@ module Blog.Contents.Main exposing (articles)
 
 import Blog.Article.Types exposing (Article)
 import Blog.Contents.Aesthetics as Aesthetics
+import Blog.Contents.Hoge as Hoge
 
 
 articles : List Article
 articles =
-    [ Aesthetics.article ]
+    [ Aesthetics.article
+    , Hoge.article
+    ]
--- src/Blog/View.elm
+++ src/Blog/View.elm
@@ -10,32 +10,77 @@ import Date
 
 root : Model -> Html Msg
 root model =
+    div []
+        [ Html.map Nav
+            (navBar model.navModel)
+        , div
+            [ class "container"
+            , class "article-container"
+            ]
+            [ div
+                [ class "content"
+                , class "container is-widescreen"
+                ]
+                (List.map summary model.articles)
+            ]
+        ]
+
+
+summary : Article.Model -> Html msg
+summary articleModel =
     let
-        summary : Article.Model -> Html msg
-        summary articleModel =
-            let
-                article =
-                    articleModel.article
+        article =
+            articleModel.article
 
-                dateString =
-                    case article.postDate of
-                        Ok date ->
-                            toString (Date.month date) ++ "/" ++ toString (Date.day date) ++ "/" ++ toString (Date.year date)
+        dateString =
+            case article.postDate of
+                Ok date ->
+                    toString (Date.month date) ++ "/" ++ toString (Date.day date) ++ "/" ++ toString (Date.year date)
 
-                        Err err ->
-                            "no date"
-            in
-                div []
-                    [ h3 [] [ text article.title ]
-                    , span [] [ text dateString ]
-                    , div []
-                        [ p [] [ text article.summary ]
-                        , a [ href ("#blog/" ++ article.id) ] [ text "read more..." ]
+                Err err ->
+                    "no date"
+    in
+        div
+            [ style
+                [ ( "margin", "0 auto" )
+                , ( "width", "80%" )
+                , ( "padding", "30px 0 0" )
+                ]
+            ]
+            [ div
+                [ class "card"
+                ]
+                [ header
+                    [ class "columns is-gapless is-vcentered"
+                    , style
+                        [ ( "box-shadow", "0 1px 2px rgba(255, 255, 255, 0.1)" )
+
+                        -- The margin-bottom of h2 is 0.5714em (em is font-size)
+                        -- and font-size is 1.75rem so, we do that calculation to vertically center everything
+                        , ( "padding", (toString <| 0.5714 * 1.75) ++ "em 5% 0" )
+                        , ( "margin-bottom", "0" )
                         ]
                     ]
-    in
-        div []
-            [ Html.map Nav
-                (navBar model.navModel)
-            , div [] (List.map summary model.articles)
+                    [ a [ class "column", href ("#blog/" ++ article.id) ]
+                        [ h2 [] [ text article.title ]
+                        ]
+                    , span
+                        [ class "column is-narrow" ]
+                        [ text dateString ]
+                    ]
+                , div
+                    [ class "card-contents"
+                    , style
+                        [ ( "padding", "1.5rem 5%" )
+                        ]
+                    ]
+                    [ p []
+                        [ text article.summary
+                        , a
+                            [ href ("#blog/" ++ article.id)
+                            ]
+                            [ text "...read more" ]
+                        ]
+                    ]
+                ]
             ]
