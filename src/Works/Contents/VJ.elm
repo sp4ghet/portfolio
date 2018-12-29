c4a777238c88273bf7b5707de13c5ba3a20eb6a7
sp4ghet <rikuo.hase1997@gmail.com>
2018-12-30 03:12:37 +0900

    hotfix typo in works/vj page

--- src/Works/Contents/VJ.elm
+++ src/Works/Contents/VJ.elm
@@ -8,7 +8,7 @@ content =
     Project "Live Visuals"
         "vj"
         "assets/img/projects/vj/square.png"
-        [ Description "I performed live visuals for DJ Baku at InfiniteRave #1 in Shibuya, Tokyo. Among the other VJs were kaiwareStyle, amagi takayosi, FMS_Cat, and sleepJam. It was a pleasure to work with these people and perform for the first time."
+        [ Description "I performed live visuals for DJ Gaku at InfiniteRave #1 in Shibuya, Tokyo. Among the other VJs were kaiwareStyle, amagi takayosi, FMS_Cat, and sleepJam. It was a pleasure to work with these people and perform for the first time."
         , Youtube "OTTJK_li17s"
-        , Description "The main system used was built using Unity along with veda.gl."
+        , Description "The main system used was built using Unity along with veda.gl. The code is published on [Github](https://youtu.be/kxnHiFZfLfk) The poster was designed by Itsuki Nomura."
         ]
