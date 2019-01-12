dd5376f9f9f8034e5f9b493aa7cbc1c7874b7bd0
Rikuo Hasegawa <8775460+sp4ghet@users.noreply.github.com>
2019-01-12 17:55:52 +0000

    Implement math and code highlighting (#16)
    * Implement math and code highlighting
    
    * try sending path to bin
    
    * Cache build progress
    
    * Fix pages moving around in order when you open them

--- src/Blog/Contents/Main.elm
+++ src/Blog/Contents/Main.elm
@@ -3,10 +3,14 @@ module Blog.Contents.Main exposing (articles)
 import Blog.Article.Types exposing (Article)
 import Blog.Contents.Lookback2018 as Lookback
 import Blog.Contents.September as September
+import Blog.Contents.TravisDeploy as TravisDeploy
+import Blog.Contents.Multiobjective as Multiobjective
 
 
 articles : List Article
 articles =
     [ Lookback.article
     , September.article
+    , TravisDeploy.article
+    , Multiobjective.article
     ]
